8f4ad07f699ca27b8bf8a23cfe736b9b5ef258c7
Okawari <noxious0510@gmail.com>
2022-08-05 17:02:42 +0800

    added base api routes

---
+++ app/Models/Anime.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Anime extends Model
+{
+    use HasFactory;
+
+    public function category() 
+    {
+        
+        return $this->belongsTo('App\Models\Category');
+        
+    }
+    
+    public function tags() 
+    {
+        
+        return $this->belongsToMany('App\Models\Tag');
+        
+    }
+}
