db704aabba075fdd6aeea7adda7de334f5d55053
Okawari <noxious0510@gmail.com>
2022-08-05 17:02:09 +0800

    added api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Anime;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,33 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/animes', function(){
+    return Anime::whereNull('posted_at')
+    ->orderBy('created_at', 'desc')
+    ->take(24)
+    ->get();
+});
+Route::get('/anime/{id}', function($id){
+    return response()->json(Anime::where('id', '=', $id)
+    ->first());
+});
+Route::get('/anime/{id}/tag', function($id){
+
+    $anime = Anime::where('id', '=', $id)->firstorFail();
+
+    $animetags = array();
+
+    foreach($anime->tags as $tag){
+        $record = $tag->name;
+        $animetags[] = $record;
+    };
+
+    return Response::json($animetags);
+
+});
+
+Route::get('/users', function(){
+    return User::orderBy('created_at', 'desc')->get();
 });
